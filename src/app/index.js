ea04af85c300824067e0470cb558c9cc432a0147
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-14 10:23:13 -0700

    :zap: Implement App component

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,33 @@ import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import Navigation from "../components/navigation/Navigation"
 import Routing from "./routing"
+import { withAuthentication } from "../session"
 
 
-const App = () => (
-    <Router>
-        <Navigation />
-        <Routing />
-    </Router>)
-export default App
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            authUser: null
+        }
+    }
+    componentDidMount() {
+        this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser ? this.setState({ authUser }) : this.setState({ authUser: null });
+        });
+    }
+    componentWillUnmount() {
+        this.listener()
+    }
+    render() {
+        return (
+            <Router>
+                <div className="bg-red ">
+                    <Navigation />
+                    <Routing />
+                </div>
+            </Router>
+        )
+    }
+}
+export default withAuthentication(App)
